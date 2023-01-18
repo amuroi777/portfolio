fcb17d190bc73b73091a2970b7b4e99531bc1cc1
amuroi777 <muroiaki7722@gmail.com>
2023-01-19 08:56:54 +0900

    _heder.scss生成

--- themes/portfolio/header.php
+++ themes/portfolio/header.php
@@ -16,12 +16,68 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+	<title><?php echo bloginfo( 'name' ); ?></title>
+
+	<link rel="stylesheet" href="./assets/css/main.css">
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+	<header class="l-header">
+		<div class="l-header__wrapper">
+			<div class="l-header__container">
+				<h1 class="header__title">
+					AKIHITO MUROI
+				</h1>
+				<nav class="l-header-nav">
+					<ul class="l-header-nav__container">
+						<li class="l-header-nav__item">
+							<a href="<?php echo esc_url( home_url() ); ?>" class="l-header-nav__link">Home</a>
+						</li>
+						<li class="l-header-nav__item">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="l-header-nav__link">Profile</a>
+						</li>
+						<li class="l-header-nav__item">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="l-header-nav__link">Skils</a>
+						</li>
+						<li class="l-header-nav__item">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="l-header-nav__link">Contact</a>
+						</li>
+					</ul>
+				</nav>
+				<div class="l-header-hamburger">
+					<button type="button" id="js-buttonHamburger" class="c-button p-hamburger"
+						aria-controls="global-nav" aria-expanded="false">
+						<span class="l-header-hamburger__line1"></span>
+						<span class="l-header-hamburger__line2"></span>
+						<span class="l-header-hamburger__line3"></span>
+						<span class="l-header-hamburger__visually-hidden"></span>
+						<!-- </span> -->
+					</button>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- /.header -->
+
+	<section class="l-drawer">
+		<nav class="l-drawer__content">
+			<ul class="l-drawer__content-items">
+				<li class="l-drawer__content-item"><a href="<?php echo esc_url( home_url() ); ?>">Home</a></li>
+				<li class="l-drawer__content-item"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Profile</a>
+				</li>
+				<li class="l-drawer__content-item"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Skils</a>
+				</li>
+				<li class="l-drawer__content-item"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Contact</a></li>
+			</ul>
+		</nav>
+	</section>
+	<!-- ./drawer-icon -->
+
+
+	<!-- 以下はデフォルト -->
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
 		<a class="skip-link screen-reader-text"
@@ -63,4 +119,4 @@
 				);
 				?>
 			</nav><!-- #site-navigation -->
-		</header><!-- #masthead -->
+		</header>
