7df7e923121486f401232614584cf62dd80119ba
amuroi777 <muroiaki7722@gmail.com>
2023-01-21 19:11:06 +0900

    footer実装

--- themes/portfolio/footer.php
+++ themes/portfolio/footer.php
@@ -11,22 +11,41 @@
 
 ?>
 
-<footer id="colophon" class="site-footer">
-	<div class="site-info">
-		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'portfolio' ) ); ?>">
-			<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'portfolio' ), 'WordPress' );
-			?>
-		</a>
-		<span class="sep"> | </span>
-		<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'portfolio' ), 'portfolio', '<a href="http://akihitomuroi.com">muroi</a>' );
-		?>
-	</div><!-- .site-info -->
-</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer class="l-footer">
+	<div class="l-footer__wrapper">
+		<div class="l-footer__container">
+			<div class="l-footer__area">
+				<div class="l-footer__title-area">
+					<h1 class="l-footer__title">
+						Contact
+					</h1>
+					<a class="l-footer__contact">お問い合わせはこちら</a>
+				</div>
+				<nav class="l-footer-nav">
+					<ul class="l-footer-nav__container">
+						<li class="l-footer-nav__item">
+							<a href="<?php echo esc_url( home_url() ); ?>" class="l-footer-nav__link">Home</a>
+						</li>
+						<li class="l-footer-nav__item">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="l-footer-nav__link">Profile</a>
+						</li>
+						<li class="l-footer-nav__item">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="l-footer-nav__link">Skils</a>
+						</li>
+						<li class="l-footer-nav__item">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="l-footer-nav__link">Contact</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div class="l-footer__bottom-area">
+				<p class="l-footer__privacy">プライバシーポリシー</p>
+				<p class="l-footer__copyright">2023,All rights reserved.</p>
+			</div>
+		</div>
+	</div>
+</footer>
+</div>
 
 <?php wp_footer(); ?>
 
