bff3b1e6341968553e4e89c5c67f3af5a8b6df84
amuroi777 <muroiaki7722@gmail.com>
2023-01-21 19:11:33 +0900

    header実装

--- themes/portfolio/header.php
+++ themes/portfolio/header.php
@@ -20,6 +20,12 @@
 	<title><?php echo bloginfo( 'name' ); ?></title>
 
 	<link rel="stylesheet" href="./assets/css/main.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Allura&family=Playfair+Display:wght@600&family=Roboto:wght@100;300;400;700&family=Satisfy&display=swap"
+		rel="stylesheet">
+
 
 	<?php wp_head(); ?>
 </head>
@@ -28,7 +34,7 @@
 	<header class="l-header">
 		<div class="l-header__wrapper">
 			<div class="l-header__container">
-				<h1 class="header__title">
+				<h1 class="l-header__title">
 					AKIHITO MUROI
 				</h1>
 				<nav class="l-header-nav">
@@ -62,7 +68,7 @@
 	</header>
 	<!-- /.header -->
 
-	<section class="l-drawer">
+	<!-- <section class="l-drawer">
 		<nav class="l-drawer__content">
 			<ul class="l-drawer__content-items">
 				<li class="l-drawer__content-item"><a href="<?php echo esc_url( home_url() ); ?>">Home</a></li>
@@ -73,50 +79,4 @@
 				<li class="l-drawer__content-item"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Contact</a></li>
 			</ul>
 		</nav>
-	</section>
-	<!-- ./drawer-icon -->
-
-
-	<!-- 以下はデフォルト -->
-	<?php wp_body_open(); ?>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text"
-			href="#primary"><?php esc_html_e( 'Skip to content', 'portfolio' ); ?></a>
-
-		<header id="masthead" class="site-header">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if ( is_front_page() && is_home() ) :
-					?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
-						rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
-						rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$portfolio_description = get_bloginfo( 'description', 'display' );
-			if ( $portfolio_description || is_customize_preview() ) :
-				?>
-				<p class="site-description">
-					<?php echo $portfolio_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-				</p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
-
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu"
-					aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'portfolio' ); ?></button>
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					)
-				);
-				?>
-			</nav><!-- #site-navigation -->
-		</header>
+	</section> -->
