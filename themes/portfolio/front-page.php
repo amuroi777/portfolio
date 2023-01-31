09692655d303fbc79f1327e34c6a492f3f2c8e64
amuroi777 <muroiaki7722@gmail.com>
2023-01-31 11:27:21 +0900

    fv修正

--- themes/portfolio/front-page.php
+++ themes/portfolio/front-page.php
@@ -14,6 +14,8 @@ get_header();
 
 <?php
 esc_html( get_template_part( 'template-parts/front-page/fv' ) );
+esc_html( get_template_part( 'template-parts/front-page/profile' ) );
+esc_html( get_template_part( 'template-parts/front-page/skils' ) );
 ?>
 
 
--- themes/portfolio/template-parts/front-page/fv.php
+++ themes/portfolio/template-parts/front-page/fv.php
@@ -11,25 +11,12 @@
 <section class="p-index-fv">
 	<div class="p-index-fv__wrapper">
 		<div class="p-index-fv__container">
-			<div class="p-index-fv__hero-area">
-				<span class="p-index-fv__hero">
-					<p class="p-index-fv__hero-text">コードを書くのが好きです。</p>
-					<p class="p-index-fv__hero-text">あなたの事業の</p>
-					<p class="p-index-fv__hero-text">体験を改善します。</p>
-				</span>
-				<div class="p-index-fv__hero-sub">
-					<p class="p-index-fv__hero-sub-text">WordPress<br>
-						Developer.
-					</p>
-				</div>
-				<div class="p-index-fv__hero-accent">
-				</div>
-				<div class="p-index-fv__hero-bottom">
-					<p class="p-index-fv__hero-bottom-text">
-						AKIHITO<br>MUROI
-					</p>
-				</div>
+			<div class="p-index-fv__hero-parent">
+				<div class="p-index-fv__box1">コードを書くのが好きです。<br class="fv-padding">あなたの事業の<br>体験を改善します。</div>
+				<div class="p-index-fv__box2">WordPress<br>Developer.</div>
+				<div class="p-index-fv__box3">AKIHITO<br>MUROI</div>
 			</div>
 		</div>
+	</div>
 	</div>
 </section>
