387040f75154dd100259120fe76fcbf6f3c102b8
amuroi777 <muroiaki7722@gmail.com>
2023-01-31 10:53:25 +0900

    skilsエリア実装

---
+++ themes/portfolio/template-parts/front-page/skils.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * トップページテンプレート(skils)
+ *
+ * @package portfolio
+ * @since 1.0.0
+ */
+
+?>
+
+<section class="p-index-skils">
+	<div class="p-index-skils__wrapper">
+		<div class="p-index-skils__container">
+			<div class="p-index-skils__title">
+				<p class="p-index-skils__title-n">02</p>
+				<p class="p-index-skils__title-main">skils</p>
+				<p class="p-index-skils__title-sub">私ができること</p>
+			</div>
+			<div class="p-index-skils__p-area">
+				<div class="p-index-skils__text-area">
+					<div class="p-index-skils__text">
+						<div class="p-index-skils__text-main">静的マークアップ</div>
+						<p class="p-index-skils__text-content"> あなたは場合とこうその発展院としてのの時を聞かますまし。
+							もう今日で自覚者はけっしてある盲従ですだろばかりを抱いでいですがは附与叱らますないで、それだけにも云わなかっでなない。
+							<br><br>
+							金力になるですのもほとんど前がまあたうた。とうてい嘉納さんを講演がたいっそ煩悶にしませ個人この学校私か講演をというご意味まいただますば
+						</p>
+					</div>
+					<div class="p-index-skils__text-bottom">
+						<div class="p-index-skils__text-main">WordPress CMS設計</div>
+						<p class="p-index-skils__text-content"> あなたは場合とこうその発展院としてのの時を聞かますまし。
+							もう今日で自覚者はけっしてある盲従ですだろばかりを抱いでいですがは附与叱らますないで、それだけにも云わなかっでなない。
+							<br><br>
+							金力になるですのもほとんど前がまあたうた。とうてい嘉納さんを講演がたいっそ煩悶にしませ個人この学校私か講演をというご意味まいただますば
+						</p>
+					</div>
+					<div class="p-index-skils__text-sub">
+						<p class="p-index-skils__text-sub-title">Tools & Langages<br>
+						<p class="p-index-skils__text-sub-content">
+							VSCODE/Docker/wp-env/npm scripts<br>
+							HTML/CSS/SCSS/ Javascript/php/
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+</section>
