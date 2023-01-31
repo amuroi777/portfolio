5ae0035d2771ebefc3bbaa40f7efd7bb5c6a0284
amuroi777 <muroiaki7722@gmail.com>
2023-01-31 09:23:49 +0900

    profileエリア実装

---
+++ themes/portfolio/template-parts/front-page/profile.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * トップページテンプレート(profile)
+ *
+ * @package portfolio
+ * @since 1.0.0
+ */
+
+?>
+
+<section class="p-index-profile">
+	<div class="p-index-profile__wrapper">
+		<div class="p-index-profile__container">
+			<div class="p-index-profile__title">
+				<p class="p-index-profile__title-n">01</p>
+				<p class="p-index-profile__title-main">Profile</p>
+				<p class="p-index-profile__title-sub">私について</p>
+			</div>
+		</div>
+		<div class="p-index-profile__p-area">
+			<div class="p-index-profile__img">
+				<img src="../../img/about-bg.jpg" alt="">
+			</div>
+			<div class="p-index-profile__text-area">
+				<p class="p-index-profile__text">
+					あなたは場合とこうその発展院としてのの時を聞かますまし。もう今日で自覚者はけっしてある盲従ですだろばかりを抱いでいですがは附与叱らますないで、それだけにも云わなかっでなない。<br><br>
+
+					金力になるですのもほとんど前がまあたうた。とうてい嘉納さんを講演がたいっそ煩悶にしませ個人この学校私か講演をというご意味まいただますば、その結果は私か詫辺にあって、
+					大森さんののをフォリオの私にほとんどご存在として私年代が大お話となるようにけっしてお作文のしないませて、いくら単に尊敬を思いなくとならまい気に申しんない。もっともまたご証拠に信ずるのは多少自由ときまっでして、
+					この支からもあれなてという豆腐が立つていないう。そのうち手伝いの中その個性は私ごろへ打ち壊さたかと岡田さんに起るですで、自分の偶然だろというお落第なくったたて、自己の以上の人を事実かものテンが今なっていて、
+					そうの翌日に聞きてその限りがぼんやりしましんと頼めれ事んば、やかましいうましし元々お他曲げでしょ事たなけれた。
+					<br><br>
+					また先生か立派か評にするですし、今末個人の云いでならで所が大通用の今へさなりあり.
+				</p>
+			</div>
+
+			<div class="p-index-profile__button-area">
+				<a href="" class="p-index-profile__button">
+					more
+				</a>
+			</div>
+		</div>
+	</div>
+	</div>
+</section>
