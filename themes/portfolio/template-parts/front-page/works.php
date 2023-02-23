a714dcaa3cf0fcbc50eb50974e87e623aa8477ed
yuki <merci.yu.k@gmail.com>
2023-02-23 19:29:39 +0900

    ♻️ 横スクロール実装

--- themes/portfolio/template-parts/front-page/works.php
+++ themes/portfolio/template-parts/front-page/works.php
@@ -10,45 +10,49 @@
 
 <section class="p-index-works">
 	<div class="p-index-works__wrapper">
-		<div class="p-index-works__container">
-			<div class="p-index-works__title">
-				<p class="p-index-works__title-n">03</p>
-				<p class="p-index-works__title-main">Works</p>
-				<p class="p-index-works__title-sub">これまで制作したもの</p>
-			</div>
-			<div class="p-index-works__card">
-				<div class="splide">
-					<div class="splide__track">
-						<ul class="splide__list">
-							<?php
-									$work_query = new WP_Query(
-										array(
-											'post_type' => 'works',
-											'posts_per_page' => -1,
-											'order'     => 'DESC',
-										)
-									);
+		<div class="p-front-section__title-inner">
+			<h2 class="c-section-title">
+				<span class="c-section-title__english">Works</span>
+				これまで制作したもの
+			</h2>
+		</div>
+		<div class="p-index-works__xscroll">
+			<div class="p-index-works__container">
+				<?php
+				$work_query = new WP_Query(
+					array(
+						'post_type'      => 'works',
+						'posts_per_page' => -1,
+						'order'          => 'DESC',
+					)
+				);
+				if ( $work_query->have_posts() ) :
+					while ( $work_query->have_posts() ) :
+						$work_query->the_post();
+						?>
+				<div class="p-index-works__column">
+					<article class="c-article-card p-index-works__card-items">
+						<a href="<?php esc_url( the_permalink() ); ?>" class="c-article-card__link p-index-works__card-item">
+							<picture class="c-article-card__image-inner">
+								<?php
+								if ( has_post_thumbnail() ) :
+									the_post_thumbnail( 'full' );
 									?>
-							<?php if ( $work_query->have_posts() ) : ?>
-								<?php while ( $work_query->have_posts() ) : ?>
-									<?php $work_query->the_post(); ?>
-
-							<li class="splide__slide">
-								<div class="p-index-works__card-items">
-									<a href="<?php esc_url( the_permalink() ); ?>" class="p-index-works__card-item">
-										<div class="card_img"><?php the_post_thumbnail( 'full' ); ?></div>
-										<div class="card_title"><?php the_title(); ?></div>
-										<div class="card_text"><?php the_content(); ?></div>
-									</a>
-								</div>
-							</li>
-
-							<?php endwhile; ?>
-							<?php endif; ?>
-							<?php wp_reset_postdata(); ?>
-
-						</ul>
-					</div>
+								<?php else : ?>
+								<img src="" alt="" class="c-article-card__noimage">
+								<?php endif; ?>
+							</picture>
+							<div class="c-article-card__content-inner">
+								<span class="c-article-card__category">tekisuto</span>
+								<h3 class="c-article-card__title"><?php the_title(); ?></h3>
+							</div>
+						</a>
+					</article>
 				</div>
+				<?php endwhile; ?>
+				<?php endif; ?>
+				<?php wp_reset_postdata(); ?>
 			</div>
+		</div>
+	</div>
 </section>
