3e4de0ac0dadeb853b0cb85613e187435e799f6e
amuroi777 <muroiaki7722@gmail.com>
2023-03-11 14:31:44 +0900

    footerリファクタリング

--- themes/portfolio/footer.php
+++ themes/portfolio/footer.php
@@ -47,12 +47,6 @@
 </footer>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
-
-<link href="
-https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css
-" rel="stylesheet">
-
 <script src="script.js" defer></script>
 
 <?php wp_footer(); ?>
