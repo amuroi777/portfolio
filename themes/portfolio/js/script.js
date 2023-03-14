e894db2ba25c031327f2e1a85f6362c9ca512e9e
yuki <merci.yu.k@gmail.com>
2023-03-14 20:08:29 +0900

    🔥 p-index-blog__containerの横スクロール幅を動的に変更

--- themes/portfolio/js/script.js
+++ themes/portfolio/js/script.js
@@ -1,12 +1,26 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   const elems = document.querySelectorAll('.p-index-blog__column');
+//   const parent = document.querySelector('.p-index-blog__container');
+
+//   let xscrollValue = 0;
+// 	let elementGap = 0;
+//   elems.forEach((elem) => {
+// 		xscrollValue += elem.clientWidth;
+// 		elementGap+=26;
+//   });
+//   parent.style.width = `${xscrollValue + elementGap}px`;
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
-  new Splide('.splide', {
-    perPage: 4, // 1ページあたりに表示するスライド数
-    perMove: 1, // スライダーを1つずつ移動させる
-    gap: '1rem', // スライドの間隔
-    breakpoints: {
-      // レスポンシブ対応
-      768: { perPage: 2, gap: '0.5rem' },
-      576: { perPage: 1, gap: '0.5rem' },
-    },
-  }).mount();
+  const parent = document.querySelector('.p-index-blog__container');
+	const elements = parent.children;
+	console.log(elements);
+	const elementsArray =[...elements];
+  let xscrollValue = 0;
+  let elementGap = 0;
+  elementsArray.forEach((elem) => {
+    xscrollValue += elem.clientWidth;
+    elementGap += 26;
+  });
+	parent.style.width = `${xscrollValue + elementGap}px`;
 });
