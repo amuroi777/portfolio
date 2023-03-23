1ccfdc6cee9cddb53455e9c1292481e8c83b7b1d
amuroi777 <muroiaki7722@gmail.com>
2023-03-23 10:41:52 +0900

    p-index-blog__containerの横スクロール幅を動的に変更

--- themes/portfolio/js/script.js
+++ themes/portfolio/js/script.js
@@ -1,4 +1,4 @@
-//ドロワー発火
+//ドロワー
 jQuery('.l-header-hamburger').on('click', function (e) {
   e.preventDefault();
 
@@ -7,4 +7,24 @@ jQuery('.l-header-hamburger').on('click', function (e) {
   // jQuery('.drawer-background').toggleClass('is-active');
 
   return false;
+});
+
+
+// p-index-blog__containerの横スクロール幅を動的に変更
+
+document.addEventListener('DOMContentLoaded', function () {
+  const parent = document.querySelector('.p-index-blog__container');
+  const elements = parent.children;
+  console.log(elements);
+  const elementsArray = [...elements];
+  
+  let xscrollValue = 0;
+  let elementGap = 0;
+  elementsArray.forEach((elem) => {
+    xscrollValue += elem.clientWidth;
+    elementGap += 26;
+  });
+
+  parent.style.width = '${xscrollValue + elementGap}px';
+
 });
