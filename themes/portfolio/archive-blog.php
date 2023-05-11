16bfb11c2d12573de3fd96068b06eab65fb35b80
amuroi777 <muroiaki7722@gmail.com>
2023-05-11 10:48:12 +0900

    circleアイコンCSSへ置き換え、レイアウト調整

--- themes/portfolio/archive-blog.php
+++ themes/portfolio/archive-blog.php
@@ -15,73 +15,70 @@
 		<div class="p-blog-title">
 			<div class="p-blog-title__wrapper">
 				<h2 class="p-blog-title__en font-italic">blog</h2>
-				<p class="p-blog-title__jp">ワークス</p>
-				<p>
-					<img class="p-blog-title__icon"
-						src="<?php echo esc_url( get_template_directory_uri() . '/img/circle.png' ); ?>" alt="">
-				</p>
+				<p class="p-blog-title__jp">ブログ</p>
+				<div class="p-blog-title__icon"></div>
 			</div>
+			<!-- ./title -->
 
-		</div>
-		<!-- ./title -->
-
-		<section class="p-blog-contents">
-			<div class="p-blog-contents__wrapper">
-				<div class="p-blog-contents__container">
-					<main class="p-blog-contents__archive-blog p-blog-contents__main">
-						<div class="p-blog-contents__breadcrumb">
-							<img class="p-blog-contents___breadcrumb-icon"
-								src="<?php echo esc_url( get_template_directory_uri() . '/img/home-icon.png' ); ?>"
-								alt="">
-							<p class="p-blog-contents__breadcrumb-padding">></p>
-							<p class="p-blog-contents__breadcrumb-title"><?php the_archive_title(); ?></p>
-						</div>
-						<?php
-						if ( have_posts() ) :
-							while ( have_posts() ) :
-								the_post();
-								?>
-						<article>
-							<a href="<?php esc_url( the_permalink() ); ?>" class="p-blog-contents__archive-blog-item">
-								<div class="p-blog-contents__archive-blog-thumbnail">
-									<?php the_post_thumbnail( 'medium_large', array( 'class' => 'p-blog-contents__archive-blog-thumbnail-image' ) ); ?>
-								</div>
-								<div class="p-blog-contents__archive-blog-body">
-									<h2 class="p-blog-contents__archive-blog-title"><?php echo $post->post_title; ?>
-									</h2>
-									<div class="p-blog-contents__meta p-blog-contents__archive-blog_meta">
-										<img class="p-blog-contents__time-icon"
-											src="<?php echo esc_url( get_template_directory_uri() . '/img/time-icon.png' ); ?>"
-											alt="">
-										<time class="p-blog-contents__meta-time"><?php the_time( 'Y.m.d' ); ?></time>
-									</div>
-								</div>
-							</a>
-						</article>
-
-
-						<?php endwhile; ?>
-						<?php else : ?>
-						<p>記事がありません</p>
-						<?php endif; ?>
-
-						<div class="pagination">
+			<section class="p-blog-contents">
+				<div class="p-blog-contents__wrapper">
+					<div class="p-blog-contents__container">
+						<main class="p-blog-contents__archive-blog p-blog-contents__main">
+							<div class="p-blog-contents__breadcrumb">
+								<img class="p-blog-contents___breadcrumb-icon"
+									src="<?php echo esc_url( get_template_directory_uri() . '/img/home-icon.png' ); ?>"
+									alt="">
+								<p class="p-blog-contents__breadcrumb-padding">></p>
+								<p class="p-blog-contents__breadcrumb-title"><?php the_archive_title(); ?></p>
+							</div>
 							<?php
-							$args = array(
-								'mid_size'           => 1,
-								'prev_text'          => '&lt;&lt;前へ',
-								'next_text'          => '次へ&gt;&gt;',
-								'screen_reader_text' => ' ',
-							);
-							the_posts_pagination( $args );
-							?>
-						</div>
+							if ( have_posts() ) :
+								while ( have_posts() ) :
+									the_post();
+									?>
+							<article>
+								<a href="<?php esc_url( the_permalink() ); ?>"
+									class="p-blog-contents__archive-blog-item">
+									<div class="p-blog-contents__archive-blog-thumbnail">
+										<?php the_post_thumbnail( 'medium_large', array( 'class' => 'p-blog-contents__archive-blog-thumbnail-image' ) ); ?>
+									</div>
+									<div class="p-blog-contents__archive-blog-body">
+										<h2 class="p-blog-contents__archive-blog-title"><?php echo $post->post_title; ?>
+										</h2>
+										<div class="p-blog-contents__meta p-blog-contents__archive-blog_meta">
+											<img class="p-blog-contents__time-icon"
+												src="<?php echo esc_url( get_template_directory_uri() . '/img/time-icon.png' ); ?>"
+												alt="">
+											<time
+												class="p-blog-contents__meta-time"><?php the_time( 'Y.m.d' ); ?></time>
+										</div>
+									</div>
+								</a>
+							</article>
 
-					</main>
+
+							<?php endwhile; ?>
+							<?php else : ?>
+							<p>記事がありません</p>
+							<?php endif; ?>
+
+							<div class="pagination">
+								<?php
+								$args = array(
+									'mid_size'           => 1,
+									'prev_text'          => '&lt;&lt;前へ',
+									'next_text'          => '次へ&gt;&gt;',
+									'screen_reader_text' => ' ',
+								);
+								the_posts_pagination( $args );
+								?>
+							</div>
+
+						</main>
+					</div>
 				</div>
-			</div>
-		</section>
-		<!-- ./sidebar -->
+			</section>
+			<!-- ./sidebar -->
 
 
 
--- themes/portfolio/archive-works.php
+++ themes/portfolio/archive-works.php
@@ -16,72 +16,72 @@
 			<div class="p-works-title__wrapper">
 				<h2 class="p-works-title__en font-italic">Works</h2>
 				<p class="p-works-title__jp">ワークス</p>
-				<p>
-					<img class="p-works-title__icon"
-						src="<?php echo esc_url( get_template_directory_uri() . '/img/circle.png' ); ?>" alt="">
-				</p>
-			</div>
-
-		</div>
-		<!-- ./title -->
-
-		<section class="p-works-contents">
-			<div class="p-works-contents__wrapper">
-				<div class="p-works-contents__container">
-					<main class="p-works-contents__archive-works p-works-contents__main">
-						<div class="p-works-contents__breadcrumb">
-							<img class="p-works-contents___breadcrumb-icon"
-								src="<?php echo esc_url( get_template_directory_uri() . '/img/home-icon.png' ); ?>"
-								alt="">
-							<p class="p-works-contents__breadcrumb-padding">></p>
-							<p class="p-works-contents__breadcrumb-title"><?php the_archive_title(); ?></p>
-						</div>
-						<?php
-						if ( have_posts() ) :
-							while ( have_posts() ) :
-								the_post();
-								?>
-						<article>
-							<a href="<?php esc_url( the_permalink() ); ?>" class="p-works-contents__archive-works-item">
-								<div class="p-works-contents__archive-works-thumbnail">
-									<?php the_post_thumbnail( 'medium_large', array( 'class' => 'p-works-contents__archive-works-thumbnail-image' ) ); ?>
-								</div>
-								<div class="p-works-contents__archive-works-body">
-									<h2 class="p-works-contents__archive-works-title"><?php echo $post->post_title; ?>
-									</h2>
-									<div class="p-works-contents__meta p-works-contents__archive-works_meta">
-										<img class="p-works-contents__time-icon"
-											src="<?php echo esc_url( get_template_directory_uri() . '/img/time-icon.png' ); ?>"
-											alt="">
-										<time class="p-works-contents__meta-time"><?php the_time( 'Y.m.d' ); ?></time>
-									</div>
-								</div>
-							</a>
-						</article>
-
-
-						<?php endwhile; ?>
-						<?php else : ?>
-						<p>記事がありません</p>
-						<?php endif; ?>
-
-						<div class="pagination">
-							<?php
-							$args = array(
-								'mid_size'           => 1,
-								'prev_text'          => '&lt;&lt;前へ',
-								'next_text'          => '次へ&gt;&gt;',
-								'screen_reader_text' => ' ',
-							);
-							the_posts_pagination( $args );
-							?>
-						</div>
-
-					</main>
+				<div class="p-works-title__icon">
 				</div>
+
 			</div>
-		</section>
-		<!-- ./sidebar -->
+			<!-- ./title -->
+
+			<section class="p-works-contents">
+				<div class="p-works-contents__wrapper">
+					<div class="p-works-contents__container">
+						<main class="p-works-contents__archive-works p-works-contents__main">
+							<div class="p-works-contents__breadcrumb">
+								<img class="p-works-contents___breadcrumb-icon"
+									src="<?php echo esc_url( get_template_directory_uri() . '/img/home-icon.png' ); ?>"
+									alt="">
+								<p class="p-works-contents__breadcrumb-padding">></p>
+								<p class="p-works-contents__breadcrumb-title"><?php the_archive_title(); ?></p>
+							</div>
+							<?php
+							if ( have_posts() ) :
+								while ( have_posts() ) :
+									the_post();
+									?>
+							<article>
+								<a href="<?php esc_url( the_permalink() ); ?>"
+									class="p-works-contents__archive-works-item">
+									<div class="p-works-contents__archive-works-thumbnail">
+										<?php the_post_thumbnail( 'medium_large', array( 'class' => 'p-works-contents__archive-works-thumbnail-image' ) ); ?>
+									</div>
+									<div class="p-works-contents__archive-works-body">
+										<h2 class="p-works-contents__archive-works-title">
+											<?php echo $post->post_title; ?>
+										</h2>
+										<div class="p-works-contents__meta p-works-contents__archive-works_meta">
+											<img class="p-works-contents__time-icon"
+												src="<?php echo esc_url( get_template_directory_uri() . '/img/time-icon.png' ); ?>"
+												alt="">
+											<time
+												class="p-works-contents__meta-time"><?php the_time( 'Y.m.d' ); ?></time>
+										</div>
+									</div>
+								</a>
+							</article>
+
+
+								<?php endwhile; ?>
+								<?php else : ?>
+							<p>記事がありません</p>
+							<?php endif; ?>
+
+							<div class="pagination">
+								<?php
+								$args = array(
+									'mid_size'           => 1,
+									'prev_text'          => '&lt;&lt;前へ',
+									'next_text'          => '次へ&gt;&gt;',
+									'screen_reader_text' => ' ',
+								);
+								the_posts_pagination( $args );
+								?>
+							</div>
+
+						</main>
+					</div>
+				</div>
+			</section>
+			<!-- ./sidebar -->
 
 
 
