3bb56dfe792dfaa5daadf2484b5856588cd3fb95
amuroi777 <muroiaki7722@gmail.com>
2023-05-27 12:16:23 +0900

    googleformとの連携、動作実装

--- themes/portfolio/page-contact.php
+++ themes/portfolio/page-contact.php
@@ -30,15 +30,17 @@
 			<div class="p-contact-item__container">
 				<div class="p-contact-item__form">
 					<h2 class="p-contact-item__form-top-title">お問い合わせフォーム</h2>
-					<form id="js-form" action="" class="p-contact-item__form-lists validationForm" novalidate>
+					<form id="js-form"
+						action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScnKv4Xi47Jt1HXf9eyygkvRRadLkrVXb694S88woQ_91-6tw/formResponse"
+						class="p-contact-item__form-lists validationForm" novalidate>
 						<dl class="p-contact-item__form-list">
 							<div class="p-contact-item__form-item">
 								<dt class="p-contact-item__form-title" data-sub="*">
 									お名前
 								</dt>
 								<dd class="p-contact-item__input">
-									<input class="required maxlength" data-maxlength="10" type="text" name="name"
-										placeholder="お名前太郎">
+									<input class="required maxlength" data-maxlength="10" type="text"
+										name="entry.1546528757" placeholder="お名前太郎" required>
 								</dd>
 							</div>
 							<div class="p-contact-item__form-item">
@@ -46,26 +48,33 @@
 									メールアドレス
 								</dt>
 								<dd class="p-contact-item__input">
-									<input class="email" type="email" id="email" name="email"
+									<input class="email" type="email" id="email" name="emailAddress"
 										placeholder="bcd@amuro.com" required>
 								</dd>
 							</div>
-
 							<div class="p-contact-item__form-item">
 								<dt class="p-contact-item__form-title">
 									お問合せ内容
 								</dt>
 								<dd class="p-contact-item__input">
-									<textarea class="query" placeholder="お問合せ内容を入力してください。"></textarea>
+									<textarea class="query" name="entry.1909066184" placeholder="お問合せ内容を入力してください。"
+										required></textarea>
 								</dd>
 							</div>
 						</dl>
 						<div class="p-contact-item__form-submit">
-							<button id="#js-submit" class="p-contact-item__button" -submit type="submit">送信する</button>
+							<button id="js-submit" class="p-contact-item__button" -submit type="submit">送信する</button>
 						</div>
 					</form>
+					<div id="js-success" class="p-contact-item__contact-message">
+						<p>送信完了しました。有難うございました。</p>
+					</div>
+					<div id="js-error" class="p-contact-item__contact-message -error">
+						<p>送信に失敗しました。ページを更新して再度送信して下さい。</p>
+					</div>
 				</div>
 			</div>
+		</div>
 	</section>
 
 	<!-- ./contact-item -->
